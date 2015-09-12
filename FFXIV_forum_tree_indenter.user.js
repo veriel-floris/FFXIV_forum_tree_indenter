c87f23ae0d3c1acb344eef1e78cba9d3797469e4
Veriel Floris <veriel.floris@gmail.com>
2015-09-13 01:40:37 +0900

    2.2 コード整理等
    Signed-off-by: Veriel Floris

--- FFXIV_forum_tree_indenter.user.js
+++ FFXIV_forum_tree_indenter.user.js
@@ -4,145 +4,136 @@
 // @description フォーラムのコメント毎にインデントをつけ、折り畳みます。
 // @include     http://forum.square-enix.com/*
 // @grant       none
-// @version     2.0
+// @version     2.2
 // ==/UserScript==
 //GM_log("FFXIV forum tree indenter");
 (function(){
+    var indent_px = 20;
+    
     // --------------------------------------------------
-    // wait load
-    window.addEventListener('load', function() {
-        var indent_px = 20;
-console.log('loaded');
-        
-        // --------------------------------------------------
-        // get tree structure
-        var postdata = [];
-        var postsummary =[];
-        get_tree_structure( postdata, postsummary );
-        
-console.log('tree analysed');
-        // --------------------------------------------------
-        // add style
-        for( i in postdata ) {
-            post = document.getElementById( "post_" + i );
-            if( post != null ) {
-                postwidth = post.clientWidth;
-                post.style.margin="0 0 0 "+postdata[i]*indent_px+"px";
-                post.style.width=postwidth-postdata[i]*indent_px+"px";
-                
-                // --------------------------------------------------
-                // get view flag
-                postdate = post.getElementsByClassName('postdate');
-                viewflag = "new";
-                if( postdate[0] != null ) {
-                    if( postdate[0].getAttribute('class') == "postdate old" ) {
-                        viewflag = "old";
-                    }
-                } else {
-                    viewflag = "del";
+    // get tree structure
+    var postdata = [];
+    var postsummary =[];
+    get_tree_structure( postdata, postsummary );
+    
+    // --------------------------------------------------
+    // add style
+    for( i in postdata ) {
+        post = document.getElementById( "post_" + i );
+        if( post != null ) {
+            postwidth = post.clientWidth;
+            post.style.margin="0 0 0 "+postdata[i]*indent_px+"px";
+            post.style.width=postwidth-postdata[i]*indent_px+"px";
+            
+            // --------------------------------------------------
+            // get view flag
+            postdate = post.getElementsByClassName('postdate');
+            viewflag = "new";
+            if( postdate[0] != null ) {
+                if( postdate[0].getAttribute('class') == "postdate old" ) {
+                    viewflag = "old";
                 }
-console.log('view flag set');
+            } else {
+                viewflag = "del";
+            }
+            
+            // --------------------------------------------------
+            // add summary to posthead
+            posthead = (post.getElementsByClassName('posthead'))[0];
+            posthead.style.whiteSpace = "nowrap";
+            
+            username = (post.getElementsByClassName('username'))[0].title;
+            usertitle = (post.getElementsByClassName('usertitle'))[0].innerHTML.trim();
+            nodecontrols = (post.getElementsByClassName('nodecontrols'))[0];
+            
+            el_username = document.createElement('b');
+            el_username.className = "username";
+            el_username.style.margin = "0 0 0 10px";
+            if( usertitle != "Player" ) {
+                el_username.style.color = "red";
+                el_username.style.font = "verdana";
+            }
+            el_username.innerHTML = username;
+            el_username_span = document.createElement('span');
+            el_username_span.appendChild( el_username );
+            
+            el_summary = document.createElement('i');
+            el_summary.style.margin = "0 0 0 10px";
+            el_summary.innerHTML = postsummary[i];
+            el_summary_span = document.createElement('span');
+            el_summary_span.appendChild( el_summary );
+            
+            if( viewflag == "del" ) {
+                posthead.appendChild( el_username_span );
+                posthead.appendChild( el_summary_span );
+            } else {
+                postdate[0].appendChild( el_username_span );
+                postdate[0].appendChild( el_summary_span );
                 
-                // --------------------------------------------------
-                // add summary to posthead
-                posthead = (post.getElementsByClassName('posthead'))[0];
-                posthead.style.whiteSpace = "nowrap";
+                el_likeitic = document.createElement('div');
+                el_likeitic.className = "likeitic";
+                el_likeitic.style.cssFloat = "right";
+                el_likeitic.style.marginRight = "50px";
+                el_likeitic.innerHTML = (post.getElementsByClassName('likeitic'))[0].innerHTML;
                 
-                username = (post.getElementsByClassName('username'))[0].title;
-                usertitle = (post.getElementsByClassName('usertitle'))[0].innerHTML.trim();
-                nodecontrols = (post.getElementsByClassName('nodecontrols'))[0];
-                
-                el_username = document.createElement('b');
-                el_username.className = "username";
-                el_username.style.margin = "0 0 0 10px";
-                if( usertitle != "Player" ) {
-                    el_username.style.color = "red";
-                    el_username.style.font = "verdana";
-                }
-                el_username.innerHTML = username;
-                el_username_span = document.createElement('span');
-                el_username_span.appendChild( el_username );
-                
-                el_summary = document.createElement('i');
-                el_summary.style.margin = "0 0 0 10px";
-                el_summary.innerHTML = postsummary[i];
-                el_summary_span = document.createElement('span');
-                el_summary_span.appendChild( el_summary );
-                
-                if( viewflag == "del" ) {
-                    posthead.appendChild( el_username_span );
-                    posthead.appendChild( el_summary_span );
-                } else {
-                    postdate[0].appendChild( el_username_span );
-                    postdate[0].appendChild( el_summary_span );
-                    
-                    el_likeitic = document.createElement('div');
-                    el_likeitic.className = "likeitic";
-                    el_likeitic.style.cssFloat = "right";
-                    el_likeitic.style.marginRight = "50px";
-                    el_likeitic.innerHTML = (post.getElementsByClassName('likeitic'))[0].innerHTML;
-                    
-                    posthead.appendChild( el_likeitic );
-                }
-console.log('port head added');
-                
-                // --------------------------------------------------
-                // collapse comments
-                if( viewflag == "old" ) {
+                posthead.appendChild( el_likeitic );
+            }
+            
+            // --------------------------------------------------
+            // collapse comments
+            if( viewflag == "old" ) {
+                postdetails = (post.getElementsByClassName('postdetails'))[0];
+                postfoot = (post.getElementsByClassName('postfoot'))[0];
+                postdetails.style.display = "none";
+                postfoot.style.display = "none";
+                posthead.className = "posthead none " + i;
+            } else if( viewflag == "del" ) {
+                // collapse deleted comment
+                userinfo = (post.getElementsByClassName('userinfo'))[0];
+                postbody = (post.getElementsByClassName('postbody'))[0];
+                userinfo.style.display = "none";
+                postbody.style.display = "none";
+                posthead.className = "posthead del none";
+            } else {
+                posthead.className = "posthead block " + i;
+            }
+            
+            // --------------------------------------------------
+            // add click event (collapse)
+            posthead.addEventListener('click', function() {
+                cl = this.getAttribute('class');
+                postclass = cl.split(/ /);
+                post = document.getElementById( "post_" + postclass[2] );
+                if( post != null ) {
                     postdetails = (post.getElementsByClassName('postdetails'))[0];
                     postfoot = (post.getElementsByClassName('postfoot'))[0];
-                    postdetails.style.display = "none";
-                    postfoot.style.display = "none";
-                    posthead.className = "posthead none " + i;
-                } else if( viewflag == "del" ) {
-                    // collapse deleted comment
-                    userinfo = (post.getElementsByClassName('userinfo'))[0];
-                    postbody = (post.getElementsByClassName('postbody'))[0];
-                    userinfo.style.display = "none";
-                    postbody.style.display = "none";
-                    posthead.className = "posthead del none";
-                }
-                
-console.log('collapse set');
-                // --------------------------------------------------
-                // add click event (collapse)
-                posthead.addEventListener('click', function() {
-                    cl = this.getAttribute('class');
-                    postclass = cl.split(/ /);
-                    post = document.getElementById( "post_" + postclass[2] );
-                    if( post != null ) {
-                        postdetails = (post.getElementsByClassName('postdetails'))[0];
-                        postfoot = (post.getElementsByClassName('postfoot'))[0];
-                        if( postclass[1] == "none" ) {
-                            postdetails.style.display = "block";
-                            postfoot.style.display = "block";
-                            this.className = "posthead block " + postclass[2];
-                        } else {
-                            postdetails.style.display = "none";
-                            postfoot.style.display = "none";
-                            this.className = "posthead none "  + postclass[2];
-                        }
+                    if( postclass[1] == "none" ) {
+                        postdetails.style.display = "block";
+                        postfoot.style.display = "block";
+                        this.className = "posthead block " + postclass[2];
+                    } else {
+                        postdetails.style.display = "none";
+                        postfoot.style.display = "none";
+                        this.className = "posthead none "  + postclass[2];
                     }
-                }, false );
-console.log('click event set');
-            }
+                }
+            }, false );
         }
-        
-        // --------------------------------------------------
-        // add menu
-        postlist_popups = document.getElementById('postlist_popups');
-        
-        postlist_popups.appendChild( menu = add_popupmenu( "popup_allopen", "全て開く" ) );
-        menu.addEventListener('click', function(){all_openclose( true )}, false );
-        
-        postlist_popups.appendChild( menu = add_popupmenu( "popup_allclose", "全て閉じる" ) );
-        menu.addEventListener('click', function(){all_openclose( false )}, false );
-        
-        postlist_popups.appendChild( menu = add_popupmenu( "popup_newdaysopen", "近日分を開く" ) );
-        menu.addEventListener('click', newdays_open, false );
-        
-console.log('menu added');
-    }, true );
+    }
+    
+    // --------------------------------------------------
+    // add menu
+    postlist_popups = document.getElementById('postlist_popups');
+    
+    postlist_popups.appendChild( menu = add_popupmenu( "popup_allopen", "全て開く" ) );
+    menu.addEventListener('click', function(){all_openclose( true )}, false );
+    
+    postlist_popups.appendChild( menu = add_popupmenu( "popup_allclose", "全て閉じる" ) );
+    menu.addEventListener('click', function(){all_openclose( false )}, false );
+    
+    postlist_popups.appendChild( menu = add_popupmenu( "popup_newdaysopen", "近日分を開く" ) );
+    menu.addEventListener('click', newdays_open, false );
+    
 })();
 
 // --------------------------------------------------
@@ -262,5 +253,3 @@ function newdays_open() {
         }
     }
 }
-
-
